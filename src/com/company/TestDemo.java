5c2b0e3e7f4bacab3fcc957112e8d48d1d967266
yudetao <ydt13147103752@163.com>
2024-06-26 18:26:39 +0800

    提交文件夹

---
+++ src/com/company/TestDemo.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class TestDemo {
+    public static String getResult(String name){
+        String result=name+"testtest";
+        return result;
+    }
+    public static void main(String[] args) {
+        getResult("test1");
+    }
+}
