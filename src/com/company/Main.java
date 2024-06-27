c80fb67dfb768ceb20affbb17861f494e6a858b8
yudetao <ydt13147103752@163.com>
2024-06-27 18:19:22 +0800

    06.27

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("aaa");
+        Cat cat=new Cat();
+        System.out.println(cat.name);
+        Cat.name="更新";
+        System.out.println(cat.name);
     }
 }
